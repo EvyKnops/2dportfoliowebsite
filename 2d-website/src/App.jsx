654684d7f5f7255478c9a23149c665971ce59489
EvyKnops <knopsevy123@gmail.com>
2026-05-12 09:31:44 +0200

    removed unnecesary code that was part of the template

--- 2d-website/src/App.jsx
+++ 2d-website/src/App.jsx
@@ -11,91 +11,27 @@ function App() {
     <>
       <section id="center">
         
-        <div>
+        <div class="SectionName">
           <h1>Evy Knops</h1>
           <p>Welcome to my Portfolio!</p>
         </div>
       </section>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
+      <section class="SectionProjects">
         
         <h1>Projects</h1>
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+
+        <div class="Project1">
+        <p>
+          Project 1
+        </p>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
+        
+        
       </section>
 
-      <div className="ticks"></div>
-      <section id="spacer">
+      <section class="SectionContact">
         <h1>Contact me!</h1>
       </section>
     </>
